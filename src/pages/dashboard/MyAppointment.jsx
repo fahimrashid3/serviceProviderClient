8af6724b66da78d548627f0775a9c9e4b71ad79c
Your Name <fahimrashid350@gmail.com>
2024-12-14 00:29:15 +0600

    appointment delete process is working

--- src/pages/dashboard/MyAppointment.jsx
+++ src/pages/dashboard/MyAppointment.jsx
@@ -1,10 +1,39 @@
 import { AiTwotoneDelete } from "react-icons/ai";
 import SectionTitle from "../../components/SectionTitle";
 import useAppointment from "../../hooks/useAppointment";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
 
 const MyAppointment = () => {
-  const [appointment] = useAppointment();
+  const [appointment, refetch] = useAppointment();
+  const axiosSecure = useAxiosSecure();
   // TODO: delete appointment automatically if time or date is over
+  const handelDeleteAppointment = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/appointments/${id}`).then((res) => {
+          if (res.data.deletedCount > 0) {
+            refetch();
+            Swal.fire({
+              position: "top-end",
+              icon: "success",
+              title: "Deleted!",
+              showConfirmButton: false,
+              timer: 1500,
+            });
+          }
+        });
+      }
+    });
+  };
   return (
     <div>
       <SectionTitle
@@ -24,6 +53,7 @@ const MyAppointment = () => {
                 <th>item</th>
                 <th>date</th>
                 <th>Time</th>
+                <th>Price (Taka)</th>
                 <th>Delete</th>
                 <th>Payment</th>
               </tr>
@@ -36,9 +66,13 @@ const MyAppointment = () => {
                   <td>{item.category}</td>
                   <td>{item.date}</td>
                   <td>{item.time}</td>
+                  <td>{item.price}</td>
 
                   <td>
-                    <button className="btn  btn-ghost btn-outline btn-error text-2xl">
+                    <button
+                      onClick={() => handelDeleteAppointment(item._id)}
+                      className="btn  btn-ghost btn-outline btn-error text-2xl"
+                    >
                       <AiTwotoneDelete />
                     </button>
                   </td>
